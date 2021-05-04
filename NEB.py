8341d4712942120e1428e471578a6acae537dbee
Marc Valdez <valdezm628@gmail.com>
2021-05-04 13:02:56 -0600

    add class for brachistochrone, think there is error in gradient

--- NEB.py
+++ NEB.py
@@ -375,12 +375,119 @@ class ConstantSlope(NEB):
 
 #}}}    
 
+#{{{ 
+def test_Brachistochrone():
+    m = 50
+    blend = np.linspace(0, 1, m)
+    X = np.outer(blend,Downhill) + np.outer(1-blend, MtEvansPeak)
+    N = Brachistochrone(X,filter = .1, k = 1e-3, projected=False)
+    N.plot()
+    N.minimize(method=None)
+    N.plot()
+    N.plot_slope()
+    print(X)
+
+class Brachistochrone(NEB):
+    def __init__(self, x, filter=None, k=.01, spring_type = 'quadratic', projected = True,mapfile = 'final.tif'
+                 ):
+        self.x , self.spring_type, self.k, self.projected = x, spring_type, k, projected
+        self.mapfile = mapfile
+        self.m = x.shape[0]
+        self.n = x.shape[1]
+        self.solved = False
+        self.eps = 1e-5
+        self.image = np.array(Image.open(self.mapfile))
+        self.original = np.copy(self.image)
+        if filter is not None:
+            numtaps = 129
+            filt = remez(numtaps, [0,filter-.02,filter+.02, .5],desired = [1,1e-4])
+            filt = np.outer(filt,filt)
+            plt.imshow(filt)
+            plt.show()
+            self.image = oaconvolve(self.image, filt,mode='same')
+        self.map = RGI([np.linspace(0,self.image.shape[1]-1,self.image.shape[1]),
+                        np.linspace(0,self.image.shape[0]-1,self.image.shape[0])],
+                        self.image.T)
+
+    def obj_cost(self, x = None):
+        if x is None: x = self.x[1:-1,:]
+        X = self.x
+        X[1:-1,:] = x.reshape(self.m-2,self.n)
+        values = self.map(X)
+        delta_h = values[0] - values[1:-1]
+        distance = np.linalg.norm(X[2:,:] - X[1:-1,:],axis=1)
+        return np.sum(distance**2 / delta_h)
+
+    def plot_slope(self):
+        x = None
+        if x is None: x = self.x[1:-1,:]
+        X = self.x
+        X[1:-1,:] = x.reshape(self.m-2,self.n)
+        values = self.map(X)
+        delta_run = np.linalg.norm(X[1:,:] - X[:-1,:],axis=1)
+        delta_rise = values[1:] - values[:-1]
+        slope = delta_rise/delta_run
+        plt.plot(slope)
+        plt.show()
+        
+    
+    def obj_gradient(self, x = None):
+        if x is None: x = self.x[1:-1,:].flatten()
+        X = self.x
+        X[1:-1,:] = x.reshape(self.m-2,self.n)
+        values = self.map(X)
+        # left/right dist here mean in index when ordered lowest to highest
+        dist_right = X[2:] - X[1:-1]
+        dist_left = X[1:-1] - X[:-2]
+        dist_left[0,:] = np.zeros((1,2))
+        distance = np.linalg.norm(X[2:,:] - X[1:-1,:],axis=1)
+        distance2 = np.vstack([distance, distance]).T
+        delta_h = values[0] - values
+        delta_h[0] = 1
+        delta_h2 = np.vstack([delta_h, delta_h]).T
+        step_right = np.vstack([self.eps * np.ones(X.shape[0]),np.zeros(X.shape[0])]).T
+        step_up    = np.vstack([np.zeros(X.shape[0]),self.eps * np.ones(X.shape[0])]).T
+        up = np.copy(X) + step_up
+        right = np.copy(X) + step_right
+        center_height = self.map(X)
+        hill_gradient = np.vstack([self.map(right) - center_height, self.map(up)- center_height]).T * 1./self.eps
+        grad_l = 2 * (dist_left / delta_h2[0:-2,:]) 
+        grad_r = -2 * (dist_right / delta_h2[1:-1,:])
+        grad = (
+                grad_l + grad_r
+                + (distance2**2 / delta_h2[1:-1,:]**2) * hill_gradient[1:-1,:]
+        ) 
+        return grad.flatten()
+
+    def minimize(self, method = None):
+        x0 = self.x[1:-1,:].flatten()
+        fun = self.cost
+        if method is None:
+            result = minimize(fun,x0,method='Nelder-Mead')
+            self.x[1:-1,:] = result.x.reshape(self.m-2,self.n)
+            self.solved = result.success
+        elif method == 'NAG':
+            result = minimize_nag(fun,x0,jac=self.gradient, stepsize = 1e-5, mom_parameter=0.1,maxiter = 1000)
+            self.x[1:-1,:] = result['x']
+        print(result)
+
+    def plot(self):
+        plt.imshow(self.original,cmap='jet')
+        plt.scatter(self.x[0,0],self.x[0,1],color='b')
+        plt.scatter(self.x[-1,0],self.x[-1,1],color='b')
+        plt.scatter(self.x[1:-1,0],self.x[1:-1,1],color='r')
+        plt.plot(self.x[:,0], self.x[:,1])
+        plt.show()
+
+#}}}    
+
 
         
 
 if __name__=='__main__':
     #test_NEB()
-    #test_Catenary()
-    test_MinMap()
-    #test_ConstantSlope()
+    # test_Catenary()
+    # test_MinMap()
+    # test_ConstantSlope()
+    test_Brachistochrone()
 
