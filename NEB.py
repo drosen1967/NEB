Added a basic LBFGS, fixed up our gradient in the minmap, got a decent initializer (not too cherry picking for minmap with both NAG and LBFGS. LBFGS is almost there, a little picky with Golden section search

NEB.py
@@ -5,11 +5,12 @@ from scipy.signal import convolve2d, remez,oaconvolve
 from scipy.interpolate import RegularGridInterpolator as RGI
 from sympy import *
 from PIL import Image
-from minimization_methods import minimize_nag
+from minimization_methods import minimize_nag, minimize_lbfgs
 import time
 
 MtEvansPeak = np.array([2866,1255])
 MtBierstadtPeak = np.array([693,1883])
+Lakeside = np.array([1093,1200])
 Downhill = np.array([3800, 2500])
 #Downhill = np.array([1312, 1371])
 #MtBierstadtPeak = np.array([3500,2500])
@@ -157,13 +158,23 @@ class Catenary(NEB):
 #}}}
 #{{{ MinMap
 def test_MinMap():
-    m = 80
-    blend = np.linspace(0, 1, m)
-    X = np.outer(blend, MtEvansPeak) + np.outer(1-blend,MtBierstadtPeak)
-    N = MinMap(X,filter = None, k = 0.5, projected=False)
+    m = 100
+    blend = np.linspace(0, 1, m//2 + 1)
+    X1 = np.outer(blend, MtEvansPeak) + np.outer(1-blend,Lakeside)
+    X2 = np.outer(blend,Lakeside) + np.outer(1-blend,MtBierstadtPeak)
+    X = np.vstack([X2[:-1,:],X1[1:,:]])
+    print(X.shape)
+    #N = MinMap(X,filter =.1, k = 8e-1, projected=True) # For NAG
+    N = MinMap(X,filter =.1, k = 1e-4, projected=True)
+    print(N.obj_cost())
     N.plot()
-    N.minimize(method='NAG')
+    pre = N.return_height()
+    N.minimize(method='BFGS')
     N.plot()
+    post = N.return_height()
+    plt.plot(pre)
+    plt.plot(post)
+    plt.show()
 
 class MinMap(NEB):
     def __init__(self, x, filter=None, k=.01, spring_type = 'quadratic', projected = True,mapfile = 'final.tif'
@@ -175,6 +186,7 @@ class MinMap(NEB):
         self.solved = False
         self.eps = 1e-5
         self.image = np.array(Image.open(self.mapfile))
+        self.original = np.copy(self.image)
         if filter is not None:
             numtaps = 129
             filt = remez(numtaps, [0,filter-.02,filter+.02, .5],desired = [1,1e-4])
@@ -192,24 +204,27 @@ class MinMap(NEB):
         X[1:-1,:] = x.reshape(self.m-2,self.n)
         values = self.map(X)
         return -np.sum(values)
-        
+
+    def return_height(self, x = None):
+        if x is None: x = self.x[1:-1,:]
+        X = self.x
+        X[1:-1,:] = x.reshape(self.m-2,self.n)
+        values = self.map(X)
+        return values  
     
     def obj_gradient(self, x = None):
         if x is None: x = self.x[1:-1,:].flatten()
         X = self.x
         X[1:-1,:] = x.reshape(self.m-2,self.n)
-        G = np.zeros_like(X)
-        g0 = self.obj_cost(X[1:-1])
-        for i in range(self.m-2):
-            e = np.zeros_like(X)
-            e[i+1,0] = self.eps
-            g1 = self.obj_cost((X + e)[1:-1,:])
-            G[i+1,0] = g1-g0
-            e[i+1,0] = 0.0
-            e[i+1,1] = self.eps
-            g1 = self.obj_cost((X + e)[1:-1,:])
-            G[i+1,1] = g1-g0
-        return G[1:-1].flatten()/self.eps
+        step_right = np.vstack([self.eps * np.ones(X.shape[0]),np.zeros(X.shape[0])]).T
+        step_up    = np.vstack([np.zeros(X.shape[0]),self.eps * np.ones(X.shape[0])]).T
+        center = np.copy(X)
+        up = np.copy(X) + step_up
+        right = np.copy(X) + step_right
+        center_height = self.map(X)
+        hill_gradient = np.vstack([self.map(right) - center_height, self.map(up)- center_height]).T * 1./self.eps
+        hill_gradient = hill_gradient[1:-1,:]
+        return -hill_gradient.flatten() * 100
 
     def minimize(self, method = None):
         x0 = self.x[1:-1,:].flatten()
@@ -219,12 +234,15 @@ class MinMap(NEB):
             self.x[1:-1,:] = result.x.reshape(self.m-2,self.n)
             self.solved = result.success
         elif method == 'NAG':
-            result = minimize_nag(fun,x0,jac=self.gradient, stepsize = 1e-5, mom_parameter=0.1)
+            result = minimize_nag(fun,x0,jac=self.gradient, stepsize = 1e-5, mom_parameter=0.1,maxiter=1000)
+            self.x[1:-1,:] = result['x']
+        elif method == 'BFGS':
+            result = minimize_lbfgs(fun,x0,jac=self.gradient, stepsize=None, alpha = .1, m = 5,maxiter=100)
             self.x[1:-1,:] = result['x']
         print(result)
 
     def plot(self):
-        plt.imshow(self.image,cmap='jet')
+        plt.imshow(self.original,cmap='jet')
         plt.scatter(self.x[0,0],self.x[0,1],color='b')
         plt.scatter(self.x[-1,0],self.x[-1,1],color='b')
         plt.scatter(self.x[1:-1,0],self.x[1:-1,1],color='r')
@@ -234,14 +252,14 @@ class MinMap(NEB):
 #}}}    
 #{{{ 
 def test_ConstantSlope():
-    m = 5000
+    m = 50
     blend = np.linspace(0, 1, m)
     X = np.outer(blend, MtEvansPeak) + np.outer(1-blend,Downhill)
-    N = ConstantSlope(X,filter = .45, k = 10e2, projected=False)
+    N = ConstantSlope(X,filter = .1, k = 2e-3, projected=False)
     elevation_change = np.abs(np.diff(N.map(np.vstack([MtEvansPeak, Downhill]))))
-    distance = np.linalg.norm(MtEvansPeak - Downhill) * 5
+    distance = np.linalg.norm(MtEvansPeak - Downhill) 
     av_slope = elevation_change/distance
-    N.goal_slope = av_slope * 1/4
+    N.goal_slope = av_slope * 1
     N.plot()
     N.minimize(method='NAG')
     N.plot()
@@ -305,7 +323,7 @@ class ConstantSlope(NEB):
         up = np.copy(X) + step_up
         right = np.copy(X) + step_right
         center_height = self.map(X)
-        hill_gradient = np.vstack([self.map(up) - center_height, self.map(right) - center_height]).T * 1./self.eps
+        hill_gradient = np.vstack([self.map(right) - center_height, self.map(up)- center_height]).T * 1./self.eps
         # Compute gradient from sheet:
         backward_point = X[:-2,:]
         forward_point = X[2:,:]
@@ -363,6 +381,6 @@ class ConstantSlope(NEB):
 if __name__=='__main__':
     #test_NEB()
     #test_Catenary()
-    #test_MinMap()
-    test_ConstantSlope()
+    test_MinMap()
+    #test_ConstantSlope()
 

minimization_methods.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#{{{ NAG
 def minimize_nag(fun, x0, args=(), jac = None, stepsize = 1e-5, 
                  mom_parameter=0.1, callback=None, ftol=1e-5, norm=np.Inf, 
                  maxiter=None, disp=False, return_all=False):
@@ -129,5 +130,148 @@ def minimize_nag(fun, x0, args=(), jac = None, stepsize = 1e-5,
         result['allvecs'] = allvecs
     
     return result
-
+#}}}
+#{{{ BF
+def minimize_lbfgs(fun, x0, args=(), jac = None, stepsize = None, 
+                 alpha = .1, m = 10,
+                 callback=None, ftol=1e-5, norm=np.Inf, 
+                 maxiter=None, disp=False, return_all=False):
     
+    """
+    Minimization of scalar function of one or more variables using Nesterov's accelerated gradient algorithm.
+    
+    Parameters
+    -----------
+    fun : callable
+        The objective function to be minimized.
+        ``fun(x, *args) -> float``
+        where `x` is an 1-D array with shape (n,) and `args`
+        is a tuple of the fixed parameters needed to completely
+        specify the function.
+        
+    x0 : ndarray, shape (n,)
+        Initial guess. Array of real elements of size (n,), 
+        where 'n' is the number of independent variables.
+        
+    args : tuple, optional
+        Extra arguments passed to the objective function and its
+        derivatives (fun and jac functions).
+    
+    jac : callable
+        The function that returns the gradient vector:
+            ``jac(x, *args) -> array_like, shape (n,)``
+        where ``x`` is an array with shape (n,) and ``args`` is a tuple with
+        the fixed parameters.
+    
+    stepsize: {float, 'linesearch'}
+        If it is a float number, the number specifies the constant stepsize.
+        If 'linesearch', the stepsize is determined by a the line search method.
+    
+    callback : callable, optional
+        Called after each iteration:
+        ``callable(xk)``
+        where ``xk`` is the current iterate.
+             
+    Options
+    -------
+    gtol : float
+        Gradient norm must be less than `gtol` before successful
+        termination.
+        
+    norm : float
+        Order of norm (Inf is max, -Inf is min).
+    
+    maxiter : int
+        Maximum number of iterations to perform.
+        
+    disp : bool
+        Set to True to print convergence messages.
+    
+    return_all : bool, optional
+        Set to True to return a list of the best solution at each of the
+        iterations.
+        
+    Returns
+    -------
+    res : dict
+        A dictionary whose keys are: ``x`` the solution array, `, ``fun`` the function value at 
+        the solution, ``gfk`` the gradient at the solution, and ``nik`` the total number of iterations.
+        If ``return_all`` is true, it also has the field ``allvecs`` that contains all ``xk`` along the 
+        iteration trajectory."""
+        
+    x0 = np.asarray(x0).flatten()
+    if maxiter is None:
+        maxiter = len(x0)*200
+    
+    # Modify the following codes to initialize the algorithm
+    k = 1
+    x = x0
+    q = jac(x)
+    sk = np.zeros([len(x),m])
+    yk = np.zeros([len(x),m])
+    pk = np.zeros([m])
+    old_grad = np.copy(q) * .9
+    old_x = np.zeros_like(x)
+    f_dif = 100
+    gr = (np.sqrt(5) + 1)/2
+    # callback(xk.reshape((xk.shape[0]//2, 2)), part=True)
+
+    # -------------------------
+    if return_all:
+        allvecs = [xk]
+    
+    while (f_dif > ftol) and (k < maxiter):
+        
+        # Modify the following codes to update xk_1, xk, yk, gfk, gnorm
+
+        q = jac(x)
+        grad = jac(x)
+        f_dif = np.linalg.norm(old_grad - grad)
+        yk = np.hstack([yk[:,1:], (grad - old_grad).reshape(-1,1)])
+        sk = np.hstack([sk[:,1:], (x - old_x).reshape(-1,1)])
+        pk = np.nan_to_num(1./(np.sum(yk * sk,axis=0) + 1e-3) * (np.linspace(m-1,0,m) <= k))
+        a_i = pk * (sk.T @ q)
+        q -= np.sum(a_i[None,:] *  yk,axis=1)
+        gamma = sk[:,-1]@yk[:,-1] / (yk[:,-1] @ yk[:,-1] + 1e-5)
+        HOk = np.eye(len(x)) * gamma
+        r = HOk @ q
+        b_i = (yk.T @ r) * pk
+        r += np.sum(sk * (a_i - b_i)[None,:],axis=1)
+        old_grad = np.copy(grad)
+        old_x = np.copy(x)
+        # Golden Section Search
+        if stepsize is None:
+            starting_step_size = 1e-1
+            a = x; b = x - starting_step_size * r
+            c = b - (b-a)/gr; d = a + (b-a)/gr
+            for i in range(40): # step precision now on the order of 4 * (2**-10)
+                if fun(c) < fun(d): b = d
+                else:           a = c
+                c = b - (b-a)/gr; d = a + (b-a)/gr
+            x = (b + a)/2 # Optimal line search step
+        else: x -= stepsize * r
+
+        print(f_dif)
+        
+        
+        # ----------------------
+        if return_all:
+            allvecs.append(xk)
+        if callback is not None:
+            callback(xk)
+            
+        
+        step = 10**(np.ceil(np.max((np.log10(maxiter)-2.0, 0))))  # show at most 100 lines of information  
+        if disp and (k % step == 0):
+            print('iter = {:4d}, feval = {:6.3f}, gnorm = {:8.6f}, alpha = {:8.6f}'.format(k, fun(xk, *args), gnorm, alpha))
+            
+        k += 1
+    
+    result = {'fun':fun(x), 'jac':jac(x), 'x':x.reshape((x.shape[0]//2, 2)), 'nit':k}
+    print(maxiter)
+    
+    if return_all:
+        result['allvecs'] = allvecs
+    
+    return result
+#}}}